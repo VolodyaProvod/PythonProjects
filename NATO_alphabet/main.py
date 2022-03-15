05dac27b0650fcf6a13f15b0f3d063f3e5d616d2
VolodyaProvod <achapovsky@gmail.com>
2022-03-16 02:32:56 +0300

    enabled exception handling

--- NATO_alphabet/main.py
+++ NATO_alphabet/main.py
@@ -1,6 +1,17 @@
 import pandas
 
 alphabet = {row.letter: row.code for (index, row) in pandas.read_csv("nato_phonetic_alphabet.csv").iterrows()}
-name = input("Input your name: ").upper()
-nato_format = [alphabet[letter] for letter in name]
-print(nato_format)
+
+
+def generate_phonetic():
+    name = input("Input your name: ").upper()
+    try:
+        nato_format = [alphabet[letter] for letter in name]
+    except KeyError:
+        print("Input your name in the correct form")
+        generate_phonetic()
+    else:
+        print(f"Your name in NATO format: {nato_format}")
+
+
+generate_phonetic()
